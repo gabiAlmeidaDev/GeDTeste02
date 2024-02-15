02b2d74a0e6ccf8cbd4ef7b51d70b6c5eaff3802
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-02-15 16:24:57 -0300

    indo pra develop

--- GeDTeste3.java
+++ GeDTeste3.java
@@ -6,6 +6,7 @@ public class GeDTeste3 {
         System.out.println("Feito na feature de novo");
         System.out.println("Feito na feature gabi2!");
         System.out.println("Feito na feature gabi2 para mostrar Daniel");
+        System.out.println("Para vermos ir pra develop");
 
 
 
